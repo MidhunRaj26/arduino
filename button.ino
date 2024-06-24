5ee649779cb7461eb211180332c8a45b058be252
MidhunRaj26 <midhunraj2672004@gmail.com>
2024-06-24 15:23:07 +0530

    Add files via upload
    Made a button using arduino

---
+++ button.ino
@@ -0,0 +1,26 @@
+// C++ code
+//
+int ledpin = 13;
+int buttonpin = 2;
+int buttonstate =0;
+ 
+void setup()
+{
+  pinMode(ledpin, OUTPUT);
+  pinMode(buttonpin,INPUT);
+    
+}
+
+void loop()
+{
+  buttonstate =digitalRead(buttonpin);
+
+  if(buttonstate == HIGH)
+  {
+    digitalWrite(ledpin,HIGH);
+  }
+  else
+  {
+    digitalWrite(ledpin,LOW);
+  }
+}
